43080146be09048d4269f3e34cfe8caca9ed34c6
Omar <omarmaaref14@gmail.com>
2025-05-19 13:47:28 +0200

    important TODO note

--- src/main/kotlin/com/dkb/urlshortner/url/repository/UrlRepository.kt
+++ src/main/kotlin/com/dkb/urlshortner/url/repository/UrlRepository.kt
@@ -5,5 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface UrlRepository: JpaRepository<DetailedUrlDB, UUID>{
-    fun findFirstByCodeOrCodeIsNull(email: String?): DetailedUrlDB?
+    /*
+     TODO : add sql index on Code column
+     This function is of O(n) complexity to improve it to O(log n )
+     we need to add an sql index on the code column ,
+     I should create a flyway migration for this.
+    */
+    fun findFirstByCodeOrCodeIsNull(code: String?): DetailedUrlDB?
 }
